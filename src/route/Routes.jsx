621900e938f69415cc8899a355edce9343e485a3
Paolo Castillo <paolo.castillo.eu@gmail.com>
2021-07-12 21:16:05 -0400

    add router

---
+++ src/route/Routes.jsx
@@ -0,0 +1,17 @@
+
+const public = {
+    path : '/',
+    name : 'home',
+    component : '',
+    children : [
+        {
+            path : '/',
+            component : '',
+        },
+        {
+            path: '/about',
+            component: '',
+        }
+    ] 
+}
+
