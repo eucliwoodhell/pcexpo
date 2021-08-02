9c740a0528717b25f4d607f1fcbbd4b698ef3208
Paolo Castillo <paolo.castillo.eu@gmail.com>
2021-08-01 20:01:53 -0400

    add see item

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { makeStyles, Container, Grid, IconButton, CardContent, Card, Typography, CardMedia, CardActions } from '@material-ui/core'
-import { useCartContext } from '../../context/CartContext'
+import { NavLink } from 'react-router-dom'
+import { makeStyles, Container, Grid, IconButton, CardContent, Card, Typography, CardMedia, CardActions, Button } from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/Delete'
+import { useCartContext } from '../../context/CartContext'
+import Alert from '../alert/Alert'
 
 const useStyles = makeStyles((theme) => ({
     content: {
@@ -24,7 +26,12 @@ const useStyles = makeStyles((theme) => ({
         width: "100%",
         paddingTop: '20.25%'
     },
+    links: {
+        textDecoration: 'none',
+        color: 'unset',
+    }
 }))
+
 
 export const Cart = () => {
     const classes = useStyles()
@@ -36,45 +43,59 @@ export const Cart = () => {
         <div className={classes.root}>
             <Container className={classes.content}>
                 <Grid container spacing={3}>
-                {cart.map((element, index) => {
-                    return <Grid item xs={12} sm={12} md={12} lg={12} key={element.id}>
-                        <Card className={classes.cart}>
-                            <CardMedia
-                                className={classes.cover}
-                                image={element.imgUrl}
-                                title="Live from space album cover"
-                            />
-                            <div className={classes.details}>
-                                <CardContent className={classes.contentCart}>
-                                    <Typography component="h5" variant="h5">
-                                        {element.name}
-                                    </Typography>
-                                    <Typography variant="subtitle1" color="textSecondary">
-                                        Cantiad : x {element.qty}
-                                    </Typography>
-                                    <Typography variant="subtitle1" color="textSecondary">
-                                        Precio : $ {element.price}
-                                    </Typography>
-                                    <br></br>
-                                    <Typography variant="body2" color="textSecondary">
-                                        Categoria: {element.category}
-                                        <br />
-                                        Marca: {element.detail[0].Brand}
-                                        <br />
-                                        Color: {element.detail[0].Color}
-                                        <br />
-                                        Tamaño: {element.detail[0].Size}
-                                    </Typography>
-                                </CardContent>
-                                <CardActions className={classes.actions}>
-                                    <IconButton edge="end" aria-label="delete" color="secondary" onClick={ () => delCart(index) }>
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </CardActions>
-                            </div>
-                        </Card>
-                    </Grid>
-                })}
+                    {!cart.length ? ( 
+                        <Grid item xs={12} sm={12} md={12} lg={12}>
+                            <Alert severity="warning">
+                                No hay producto en el carrito para completar la compra!
+                                <NavLink to="/product" key="/product" className={classes.links} >
+                                    <Button color="primary">
+                                        Ver Productos
+                                    </Button>
+                                </NavLink>
+                            </Alert>
+                        </Grid>
+                    ):(
+                        null
+                    )}
+                    {cart.map((element, index) => {
+                        return <Grid item xs={12} sm={12} md={12} lg={12} key={element.id}>
+                            <Card className={classes.cart}>
+                                <CardMedia
+                                    className={classes.cover}
+                                    image={element.imgUrl}
+                                    title="Live from space album cover"
+                                />
+                                <div className={classes.details}>
+                                    <CardContent className={classes.contentCart}>
+                                        <Typography component="h5" variant="h5">
+                                            {element.name}
+                                        </Typography>
+                                        <Typography variant="subtitle1" color="textSecondary">
+                                            Cantiad : x {element.qty}
+                                        </Typography>
+                                        <Typography variant="subtitle1" color="textSecondary">
+                                            Precio : $ {element.price}
+                                        </Typography>
+                                        <br></br>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Categoria: {element.category}
+                                            <br />
+                                            Marca: {element.detail[0].Brand}
+                                            <br />
+                                            Color: {element.detail[0].Color}
+                                            <br />
+                                            Tamaño: {element.detail[0].Size}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions className={classes.actions}>
+                                        <IconButton edge="end" aria-label="delete" color="secondary" onClick={ () => delCart(index) }>
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </CardActions>
+                                </div>
+                            </Card>
+                        </Grid>
+                    })}
                 </Grid>
             </Container>
         </div>
