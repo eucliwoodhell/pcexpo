683f320f0060cfe832aab02cefb1224fc6b825e5
Paolo Castillo <paolo.castillo.eu@gmail.com>
2021-08-11 20:49:41 -0400

    add component control [select, checkbox, radio, datepicker]

--- src/components/controls/Control.jsx
+++ src/components/controls/Control.jsx
@@ -1,9 +1,13 @@
 import Input from './Input'
 import Button from './Button'
+import Checkbox from './Checkbox'
+import DatePicker from './DatePicker'
+import RadioGroup from './RadioGroup'
+import Select from './Select'
 
 const Control ={
     Input,
-    Button    
+    Button,
 }
 
 export default Control
